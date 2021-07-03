4c7c1db2dd50ff9ed6f8a9af944dc8ca6405ad6e
yuanzhenrong <86541595+yuanzhenrong@users.noreply.github.com>
2021-07-03 09:06:19 +0800

    pull-commin

--- src/main/java/test1/test11.java
+++ src/main/java/test1/test11.java
@@ -9,5 +9,6 @@ public class test11 {
         System.out.println("hello5");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
